e4f3df7b01618a8dd7321acc5816a03bb52f7bca
Levi Cook <levicook@gmail.com>
2021-08-07 23:32:00 -0600

    instruction serialization (with Borsh)

--- 001-counter/program/src/instruction.rs
+++ 001-counter/program/src/instruction.rs
@@ -1,3 +1,6 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq)]
 pub enum CounterInstruction {
     /// Increments the counter by 1
     ///
@@ -9,7 +12,7 @@ pub enum CounterInstruction {
     ///
     /// Accounts expected:
     /// 0. `[signer]` Account of the person decrementing the counter.
-    IncrementBy { amount: i64 },
+    IncrementBy { amount: u16 },
 
     /// Decrements the counter by 1
     ///
@@ -21,5 +24,43 @@ pub enum CounterInstruction {
     ///
     /// Accounts expected:
     /// 0. `[signer]` Account of the person decrementing the counter.
-    DecrementBy { amount: i64 },
+    DecrementBy { amount: u16 },
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::instruction::CounterInstruction;
+    use borsh::{BorshDeserialize, BorshSerialize};
+
+    #[test]
+    fn test_increment_serialization() {
+        let original = CounterInstruction::Increment;
+        let encoded = original.try_to_vec().expect("Could not encode");
+        let decoded = CounterInstruction::try_from_slice(&encoded).expect("Could not decode");
+        assert_eq!(original, decoded);
+    }
+
+    #[test]
+    fn test_increment_by_serialization() {
+        let original = CounterInstruction::IncrementBy { amount: 10 };
+        let encoded = original.try_to_vec().expect("Could not encode");
+        let decoded = CounterInstruction::try_from_slice(&encoded).expect("Could not decode");
+        assert_eq!(original, decoded);
+    }
+
+    #[test]
+    fn test_decrement_serialization() {
+        let original = CounterInstruction::Decrement;
+        let encoded = original.try_to_vec().expect("Could not encode");
+        let decoded = CounterInstruction::try_from_slice(&encoded).expect("Could not decode");
+        assert_eq!(original, decoded);
+    }
+
+    #[test]
+    fn test_decrement_by_serialization() {
+        let original = CounterInstruction::DecrementBy { amount: 10 };
+        let encoded = original.try_to_vec().expect("Could not encode");
+        let decoded = CounterInstruction::try_from_slice(&encoded).expect("Could not decode");
+        assert_eq!(original, decoded);
+    }
 }
